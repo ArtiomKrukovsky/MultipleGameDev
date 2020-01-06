4f0bb04d42662f54b7583edda0d3aa7572d01464
Artiom Krukovsky <Artiomgood1@gmail.com>
2020-01-06 16:31:14 +0300

    fix: bug with user join and add move user script

---
+++ Assets/Scrips/Hero/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSpeed = 3f;
+
+    private PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRotation, 0f) * lookSpeed;
+
+        motor.Rotate(rotation);
+
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        Vector3 camRotation = new Vector3(xRotation, 0f, 0f) * lookSpeed;
+
+        motor.RotateCam(camRotation);
+    }
+}
---
+++ Assets/Scrips/Hero/PlayerMotor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField] 
+    private Camera camera;
+
+    private Rigidbody rigidbody;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 rotationCamera = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCam(Vector3 _rotationCamera)
+    {
+        rotationCamera = _rotationCamera;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMove();
+    }
+
+    void PerformMove()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(rotation));
+        if (camera != null)
+        {
+            camera.transform.Rotate(rotationCamera);
+        }
+    }
+}
